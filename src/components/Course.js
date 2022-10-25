4a25603ecae37ee3ba342243c215d2b823aebcb4
ahasanUllah <bulbulahasan@gmail.com>
2022-10-25 14:51:05 +0600

    Home page added

---
+++ src/components/Course.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Course = () => {
+    return (
+        <div>
+            <h2>Thsi si course page</h2>
+        </div>
+    );
+};
+
+export default Course;
--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -1,8 +1,18 @@
 
 import { createBrowserRouter } from 'react-router-dom'
+import Course from '../Course'
 import Header from '../Header'
+import Home from '../Home'
+import Main from '../layout/Main'
 export const router = createBrowserRouter([
 {
-    path:'/', element: <Header></Header>
+    path:'/', element: <Main></Main>, children: [
+        {
+            path:'/', element: <Home></Home>
+        },
+        {
+            path:'/course', element: <Course></Course>
+        }
+    ]
 }
 ])
